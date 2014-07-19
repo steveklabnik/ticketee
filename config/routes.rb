a3ec8c0c261e5ecceda6fde65b1e19ea420abfd2
Steve Klabnik <steve@steveklabnik.com>
2014-07-19 14:23:21 -0400

    Implemented sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Ticketee::Application.routes.draw do
+  get "users/new"
+  get "users/create"
+  get "users/show"
   root "projects#index"
 
   resources :projects do
     resources :tickets
   end
+
+	resources :users
 end
